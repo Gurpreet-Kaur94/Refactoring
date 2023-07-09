857e4747a304198be7e16806596ae8ca618d356d
Gurpreet Kaur <gurpreekaur@expediagroup.com>
2023-07-09 17:57:00 +0530

    before refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,53 @@
 package org.example;
 
+import org.example.chapter1.Customer;
+import org.example.chapter1.Movie;
+import org.example.chapter1.Rental;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Customer customer = new Customer("Manreet");
+        Movie newMovie = new Movie("Tume mile", 1);
+        Movie regular = new Movie("Ajnabi", 0);
+        Movie children = new Movie("Pikachu", 2);
+        Rental newMovieRental = new Rental(newMovie, 2);
+        Rental regularRntal = new Rental(regular, 2);
+        Rental childrenRntal = new Rental(children, 3);
+        customer.addRental(newMovieRental);
+        customer.addRental(regularRntal);
+        customer.addRental(childrenRntal);
+
+       String manreetStatement = customer.statement();
+        System.out.println(manreetStatement);
+       String expectedStatement = "Rental Record for Manreet\n\tTume mile\t6.0\n\tAjnabi\t2.0\n\tPikachu\t1.5\nAmount owed is 9.5\nYou earned 4 frequent renter points";
+        System.out.println(manreetStatement.equals(expectedStatement));
+
+        Customer raman = new Customer("raman");
+        newMovieRental = new Rental(newMovie, 3);
+        regularRntal = new Rental(regular, 1);
+        childrenRntal = new Rental(children, 4);
+        raman.addRental(newMovieRental);
+        raman.addRental(regularRntal);
+        raman.addRental(childrenRntal);
+
+        String ramanStatement = raman.statement();
+        System.out.println(ramanStatement);
+        expectedStatement = "Rental Record for raman\n\tTume mile\t9.0\n\tAjnabi\t2.0\n\tPikachu\t3.0\nAmount owed is 14.0\nYou earned 4 frequent renter points";
+        System.out.println(ramanStatement.equals(expectedStatement));
+
+        raman = new Customer("raman");
+        newMovieRental = new Rental(newMovie, 1);
+        regularRntal = new Rental(regular, 1);
+        childrenRntal = new Rental(children, 4);
+        raman.addRental(newMovieRental);
+        raman.addRental(regularRntal);
+        raman.addRental(childrenRntal);
+
+        ramanStatement = raman.statement();
+        System.out.println(ramanStatement);
+        expectedStatement = "Rental Record for raman\n\tTume mile\t3.0\n\tAjnabi\t2.0\n\tPikachu\t3.0\nAmount owed is 8.0\nYou earned 3 frequent renter points";
+        System.out.println(ramanStatement.equals(expectedStatement));
+
+
     }
 }
---
+++ src/main/java/org/example/chapter1/Customer.java
@@ -0,0 +1,59 @@
+package org.example.chapter1;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Customer {
+
+    private String name;
+    private List<Rental> rentals = new LinkedList<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addRental(Rental rental) {
+        this.rentals.add(rental);
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+        for(Rental rental: rentals) {
+            double thisAmount = 0;
+            switch (rental.getMovie().getPriceCode()) {
+                case Movie.REGULAR -> {
+                    thisAmount += 2;
+                    if (rental.getDaysRented() > 2 ) {
+                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                    }
+                }
+                case Movie.NEW_RELEASE -> {
+                    thisAmount += rental.getDaysRented()* 3;
+                }
+                case Movie.CHILDRENS -> {
+                    thisAmount += 1.5;
+                    if (rental.getDaysRented() > 3 ) {
+                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                    }
+                }
+            }
+            frequentRenterPoints += 1;
+            if(rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
+                frequentRenterPoints += 1;
+            }
+            result += "\t" + rental.getMovie().getTitle() + "\t" +
+                    (thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+        result += "Amount owed is " + (totalAmount) + "\n";
+        result += "You earned " + (frequentRenterPoints) +
+                " frequent renter points";
+        return result;
+    }
+}
